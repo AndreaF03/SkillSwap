a2801a72bbfb04e47796f395f6d6b4f826a408b8
AndreaF03 <andreafdes034820@gmail.com>
2026-06-10 12:30:50 +0530

    Configure frontend API for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+export default axios.create({
+  baseURL: "https://skillswap-eu6r.onrender.com/api/",
 });
-
-export default api;
